692418ef5a0c7c982f35510c7ca2a068970363ee
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-19 03:04:26 +0400

    Simple design

--- Anket2/Form1.Designer.cs
+++ Anket2/Form1.Designer.cs
@@ -28,20 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelAnket = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelBirthDate = new System.Windows.Forms.Label();
+            this.maskedTbPhone = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTbBirthDate = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBFilename = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // labelAnket
+            // 
+            this.labelAnket.Font = new System.Drawing.Font("Rockwell", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAnket.ForeColor = System.Drawing.Color.Black;
+            this.labelAnket.Location = new System.Drawing.Point(46, 32);
+            this.labelAnket.Name = "labelAnket";
+            this.labelAnket.Size = new System.Drawing.Size(373, 378);
+            this.labelAnket.TabIndex = 0;
+            this.labelAnket.Text = "Anket";
+            this.labelAnket.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelAnket.UseCompatibleTextRendering = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(532, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 274);
+            this.listBox1.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelName.Location = new System.Drawing.Point(85, 112);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(100, 23);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Name";
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSurname.Location = new System.Drawing.Point(85, 158);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(100, 23);
+            this.labelSurname.TabIndex = 2;
+            this.labelSurname.Text = "Surname";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelEmail.Location = new System.Drawing.Point(85, 200);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(100, 23);
+            this.labelEmail.TabIndex = 2;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPhone.Location = new System.Drawing.Point(85, 243);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(100, 23);
+            this.labelPhone.TabIndex = 2;
+            this.labelPhone.Text = "Phone";
+            // 
+            // labelBirthDate
+            // 
+            this.labelBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBirthDate.Location = new System.Drawing.Point(85, 288);
+            this.labelBirthDate.Name = "labelBirthDate";
+            this.labelBirthDate.Size = new System.Drawing.Size(100, 23);
+            this.labelBirthDate.TabIndex = 2;
+            this.labelBirthDate.Text = "BirthDate";
+            // 
+            // maskedTbPhone
+            // 
+            this.maskedTbPhone.Location = new System.Drawing.Point(191, 242);
+            this.maskedTbPhone.Mask = "(+\\9\\94) 00-000-00-00";
+            this.maskedTbPhone.Name = "maskedTbPhone";
+            this.maskedTbPhone.Size = new System.Drawing.Size(100, 20);
+            this.maskedTbPhone.TabIndex = 3;
+            // 
+            // maskedTbBirthDate
+            // 
+            this.maskedTbBirthDate.Location = new System.Drawing.Point(191, 287);
+            this.maskedTbBirthDate.Mask = "00/00/0000";
+            this.maskedTbBirthDate.Name = "maskedTbBirthDate";
+            this.maskedTbBirthDate.Size = new System.Drawing.Size(100, 20);
+            this.maskedTbBirthDate.TabIndex = 3;
+            this.maskedTbBirthDate.ValidatingType = typeof(System.DateTime);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(192, 112);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(192, 157);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 4;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(191, 199);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 4;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonChange.Location = new System.Drawing.Point(295, 328);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(90, 32);
+            this.buttonChange.TabIndex = 5;
+            this.buttonChange.Text = "Change";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(295, 366);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 32);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBFilename
+            // 
+            this.textBFilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBFilename.ForeColor = System.Drawing.Color.Gray;
+            this.textBFilename.Location = new System.Drawing.Point(532, 312);
+            this.textBFilename.Name = "textBFilename";
+            this.textBFilename.Size = new System.Drawing.Size(193, 22);
+            this.textBFilename.TabIndex = 4;
+            this.textBFilename.Text = "Filename";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBFilename);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.maskedTbBirthDate);
+            this.Controls.Add(this.maskedTbPhone);
+            this.Controls.Add(this.labelBirthDate);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelAnket);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAnket;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelBirthDate;
+        private System.Windows.Forms.MaskedTextBox maskedTbPhone;
+        private System.Windows.Forms.MaskedTextBox maskedTbBirthDate;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBFilename;
     }
 }
 
--- Anket2/Form1.cs
+++ Anket2/Form1.cs
@@ -17,5 +17,10 @@ namespace Anket2
             InitializeComponent();
                         
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
